aec78c1f83f5c1174f2884e09db9f111e34d15ea
dhanshri <112890281+dhanshri888@users.noreply.github.com>
2022-09-16 15:38:15 +0530

    Add files via upload
    assignment question-2

---
+++ _2_word_from_user.py
@@ -0,0 +1,8 @@
+# write a python program that accepts the word from the user and reverce it.
+#sample test case
+# input : Edyoda
+# output : adoydE
+
+
+word = input("Enter the word to reverse:")
+print("name in mirror dimension:",word[-1::-1])
